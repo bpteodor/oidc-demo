5f71d7af00c6cfa781708caa84feb5eb1fd9af59
Teodor Bran <teodor.bran@ic-consult.com>
2026-03-28 21:22:54 +0100

    rm

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
-import axios from "axios"
 import router from './components/routes'
 
 import App from './App.vue'
@@ -12,8 +11,6 @@ declare global {
         tokenChecked: boolean   // true if the token was checked for validity
     }
 }
-
-axios.defaults.timeout = window.APP_CONFIG.axios.defaults.timout
 
 // initialize app
 export const app = createApp(App)
